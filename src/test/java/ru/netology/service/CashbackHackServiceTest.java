f01c3caa3d17a54fa1e136206c9b01c4e73fdee6
Maxim Kazantsev <aeontal@gmail.com>
2021-04-12 01:30:44 +0500

    junit4 update#1 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,10 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.testng.Assert.*;
+//import org.testng.annotations.Test;
+//import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
@@ -14,25 +16,25 @@ public class CashbackHackServiceTest {
     public void shouldCashBackBoundaryMoreThanThousand() {
         int actual = service.remain(1130);
         int expect = 870;
-        assertEquals(actual, expect);
+        assertEquals(expect,actual);
     }
 
     @Test
     public void shouldCashBackEqualLimits() {
         int actual = service.remain(1000);
         int expect = 0;
-        assertEquals(actual, expect);
+        assertEquals(expect,actual);
     }
     @Test
     public void shouldCashBackEqualZero() {
         int actual = service.remain(0);
         int expect = 1000;
-        assertEquals(actual, expect);
+        assertEquals(expect,actual);
     }
     @Test
     public void shouldCashBackLessThenThousand() {
         int actual = service.remain(550);
         int expect = 450;
-        assertEquals(actual, expect);
+        assertEquals(expect,actual);
     }
 }
